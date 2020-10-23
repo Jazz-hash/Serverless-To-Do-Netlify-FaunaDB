3a79d6f0ee0e843316443b16cde8e3bbe9a2f372
Jazz-hash <jazzelmehmood6@gmail.com>
2020-10-23 14:29:20 +0500

    still fixing

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -6,7 +6,27 @@ import { Link } from "gatsby";
 
 let Dash = () => {
   const user = useContext(IdentityContext);
-  return <div>Dash hasUser: {user && user.user_metadata.full_name}</div>;
+  console.log(user);
+  return (
+    <Container>
+      <Flex as="nav">
+        <NavLink as={Link} to={"/"} p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={"/app"} p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink to="#!" p={2}>
+            {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <div>Dash hasUser: {user && user.user_metadata.full_name}</div>
+      </Flex>
+    </Container>
+  );
 };
 
 let DashLoggedOut = () => {
