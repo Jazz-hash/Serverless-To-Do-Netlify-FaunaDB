b1200edd5549564eb854c3819d8a9dc7ca331a6c
Jazz-hash <jazzelmehmood6@gmail.com>
2020-10-23 15:29:32 +0500

    Testing with apollo & graphql

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -1,40 +1,8 @@
 import React, { useContext } from "react";
 import { Router } from "@reach/router";
 import { IdentityContext } from "../../identity-context";
-import { Button, Container, Flex, Heading, NavLink } from "theme-ui";
-import { Link } from "gatsby";
-
-let Dash = () => {
-  const { user, identity: netlifyIdentity } = useContext(IdentityContext);
-
-  console.log(user, user.user_metadata);
-  return (
-    <Container>
-      <Flex as="nav">
-        <NavLink as={Link} to={"/"} p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to={"/app"} p={2}>
-          Dashboard
-        </NavLink>
-        {user && (
-          <NavLink
-            to="#!"
-            p={2}
-            onClick={() => {
-              netlifyIdentity.logout();
-            }}
-          >
-            Logout {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
-      <Flex sx={{ flexDirection: "column", padding: 3 }}>
-        <div>Dash hasUser: {user && user.user_metadata.full_name}</div>
-      </Flex>
-    </Container>
-  );
-};
+import { Button, Container, Flex, Heading } from "theme-ui";
+import Dash from "../components/dashboard";
 
 let DashLoggedOut = () => {
   const { identity: netlifyIdentity } = useContext(IdentityContext);
